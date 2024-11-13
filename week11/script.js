21a457d239f4c616f738f61705198dd7fbd9d57f
yifanz <yifanzhu773@gmail.com>
2024-11-12 19:00:12 -0800

    updated assignment 11

--- week11/script.js
+++ week11/script.js
@@ -1,29 +1,31 @@
-let message;
-
-
 const today = new Date();
 const month = today.getMonth();
 const day = today.getDate();
 const year = today.getFullYear();
 
-
-document.getElementById("date").innerHTML = month + 1 + "/" + day + "/" + year;
-
-
 const bday = new Date(1997, 11, 12);
 const bd_month = bday.getMonth();
 const bd_day = bday.getDate();
+const bd_year = bday.getFullYear();
+
+const todayDate = month + 1 + "/" + day + "/" + year;
+const birthDate = bd_month + 1 + "/" + bd_day + "/" + bd_year;
+
 const imgElement = document.getElementById('emoji');
+
+birthdayMsg = "Message";
 
 
 if ( month == bd_month && day == bd_day ) {
-    message = "Happy Birthday!"
-    document.getElementById("message").innerHTML = "Happy Birthday!";
-    document.getElementById("today").innerHTML = "Your birthday is ";
+    birthdayMsg = "Happy Birthday!"
     imgElement.src = 'happy.webp';
 } else {
-    document.getElementById("message").innerHTML = "It's not your birthday yet.";
+    birthdayMsg = "It's not your birthday yet."
     imgElement.src = 'sad-emoji.png';
 }
 
+
+document.getElementById("date").innerHTML = todayDate;
+document.getElementById("dob").innerHTML = birthDate;
+document.getElementById("message").innerHTML = birthdayMsg;
 console.log(message);
