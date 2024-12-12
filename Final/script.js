26f004b669a99957c4d268175eb2f98e8d566778
yifanz <yifanzhu773@gmail.com>
2024-12-11 22:36:09 -0800

    final version of final

--- Final/script.js
+++ Final/script.js
@@ -1,8 +1,36 @@
 
 
+
 function display(){
-    dispWin = window.open('certificate.html', 'NewWin',
-        'toolbar=no,status=no,titlebar=no,width=800,height=600')    
     awardee = document.form1.name.value;
-    document.form1.name.value = "";
+    if  (awardee != "") {
+        dispWin = window.open('certificate.html', 'NewWin',
+            'toolbar=no,status=no,titlebar=no,width=800,height=600')    
+        
+        document.form1.name.value = "";
+    } else {
+        alert("You must enter a name. Please try again.");
+    }
+    
 }
+
+function displayPop(){
+    awardee = document.form2.name.value;
+    if  (awardee != "") {
+        dispWin = window.open('certificate.html', 'NewWin',
+            'toolbar=no,status=no,titlebar=no,width=800,height=600')    
+        
+        document.form2.name.value = "";
+    } else {
+        alert("You must enter a name. Please try again.");
+    }
+}
+
+
+function openForm() {
+    document.getElementById("form2").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("form2").style.display = "none";
+  } 
